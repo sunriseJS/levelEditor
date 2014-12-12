464f343c804ceb073ded263833c5ab86683ee728
JGerdes <jonas.gerdes@gmx.net>
2014-12-12 12:18:30 +0200

    extended entityTypeEditor

--- js/entitymanager.js
+++ js/entitymanager.js
@@ -2,13 +2,18 @@ sunriseEditor.entitymanager = (function(){
 	
 	var listDom,
 		editorDom,
+		editor = {},
 		entityTypes = {},
 		entities = [];
+
 
 	var _this = {
 		init : function(){	
 			listDom = document.querySelector('#entity-selector > ul');
 			editorDom = document.querySelector('#entity-editor');
+			editor.name = editorDom.querySelector('input[name=name]');
+			editor.width = editorDom.querySelector('input[name=width]');
+			editor.height = editorDom.querySelector('input[name=height]');
 
 			var fab = document.querySelector('#entity-selector > .fab');
 			fab.addEventListener('click', function(){
@@ -29,6 +34,12 @@ sunriseEditor.entitymanager = (function(){
 			var e = new Entity(name);
 			e.appendTo(listDom);
 			entityTypes[name] = e;
+		},
+		editEntity : function(entity){
+			editorDom.style.display = 'block';
+			editor.name.value = entity.name;
+			editor.width.value = entity.width;
+			editor.height.value = entity.height;
 		}
 	};
 
@@ -39,14 +50,23 @@ sunriseEditor.entitymanager = (function(){
 
 function Entity(name){
 	this.name = name;
+	this.width = 0;
+	this.height = 0;
 	this.components = {};
 }
 
 
 Entity.prototype.appendTo = function(element){
+	var _this = this;
 	var data = 	'<li class="card clear">'+
 					'<div class="info">'+this.name+'</div>'+
 					'<div class="preview"></div>'+
 				'</li>';
 	element.innerHTML += data;
+	element.childNodes[element.childNodes.length-1].addEventListener('click', (function(scope){
+		return (function(){
+			sunriseEditor.entitymanager.editEntity(scope);
+			console.log(scope);
+		});
+	})(this));
 };
